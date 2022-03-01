109ba066dd10f5d2bdd48c0ee3e4035c09d6daf9
David Cottrell <cottrell@users.noreply.github.com>
2022-03-01 17:28:19 +0000

    Remove typing and future.

--- src/npx/_unique.py
+++ src/npx/_unique.py
@@ -1,70 +1,40 @@
-from __future__ import annotations
-
-from typing import Callable
-
+# from __future__ import annotations
 import numpy as np
-from numpy.typing import ArrayLike
+# from numpy.typing import ArrayLike
 
 
-def _unique_tol(
-    unique_fun: Callable, a: ArrayLike, tol: float, **kwargs
-) -> np.ndarray | tuple[np.ndarray, ...]:
+def _unique_tol(unique_fun, a, tol, **kwargs):
     a = np.asarray(a)
-    # compute 1/tol first. Difference:
-    #
-    #   int(3.3 / 0.1) = int(32.99999999999999) = 32
-    #   int(3.3 * (1.0 / 0.1)) = int(33.0) = 33
-    #
     aint = (a * (1.0 / tol)).astype(int)
-
-    return_index = kwargs.pop("return_index", False)
-
+    return_index = kwargs.pop('return_index', False)
     _, idx, *out = unique_fun(aint, return_index=True, **kwargs)
     unique_a = a[idx]
-
     if return_index:
         out = [idx, *out]
-
     if len(out) == 0:
         return unique_a
+    return unique_a, *out
 
-    return (unique_a, *out)
 
-
-def unique(
-    a: ArrayLike, tol: float = 0.0, **kwargs
-) -> np.ndarray | tuple[np.ndarray, ...]:
+def unique(a, tol=0.0, **kwargs):
     assert tol >= 0.0
     if tol > 0.0:
         return _unique_tol(np.unique, a, tol, **kwargs)
-
     return np.unique(a, **kwargs)
 
 
-def unique_rows(
-    a: ArrayLike,
-    return_index: bool = False,
-    return_inverse: bool = False,
-    return_counts: bool = False,
-) -> np.ndarray | tuple[np.ndarray, ...]:
-    # The numpy alternative `np.unique(a, axis=0)` is slow; cf.
-    # <https://github.com/numpy/numpy/issues/11136>.
+def unique_rows(a, return_index=False, return_inverse=False, return_counts=
+    False):
     a = np.asarray(a)
-
     a_shape = a.shape
     a = a.reshape(a.shape[0], np.prod(a.shape[1:], dtype=int))
-
-    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize * a.shape[1])))
-    out = np.unique(
-        b,
-        return_index=return_index,
-        return_inverse=return_inverse,
-        return_counts=return_counts,
-    )
-    # out[0] are the sorted, unique rows
+    b = np.ascontiguousarray(a).view(np.dtype((np.void, a.dtype.itemsize *
+        a.shape[1])))
+    out = np.unique(b, return_index=return_index, return_inverse=
+        return_inverse, return_counts=return_counts)
     if isinstance(out, tuple):
-        out = (out[0].view(a.dtype).reshape(out[0].shape[0], *a_shape[1:]), *out[1:])
+        out = out[0].view(a.dtype).reshape(out[0].shape[0], *a_shape[1:]
+            ), *out[1:]
     else:
         out = out.view(a.dtype).reshape(out.shape[0], *a_shape[1:])
-
     return out
